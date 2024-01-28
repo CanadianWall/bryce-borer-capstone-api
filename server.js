00eaf9035854c5c98070c06aeb66508a9f58ce36
Bryce Borer <bryce.borer@gmail.com>
2024-01-28 11:35:59 -0500

    fixed a bug that caused issues when uploading a picture after one was already there. Also added more sample reply JSON files

--- server.js
+++ server.js
@@ -2,52 +2,51 @@ const express = require('express');
 const app = express();
 const port = process.env.REACT_APP_PORT || 8080;
 const jwt = require("jsonwebtoken");
-const FOOD_URL = process.env.REACT_APP_FOOD_URL;
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const FOOD_URL = process.env.REACT_APP_FOOD_URL;
+// const API_KEY = process.env.API_KEY;
 const tempFood = require ('./sampleOutput/sandwich.json')
 
-// const { PORT, CORS_ORIGIN, API_KEY, FOOD_URL} = process.env
+const { PORT, CORS_ORIGIN, API_KEY, FOOD_URL, JSON_SECRET_KEY} = process.env
 
 const cors = require('cors');
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+// const multer = require('multer')
+// const upload = multer({ dest: 'uploads/' })
 
 app.use(express.json());
 app.use(cors());
-const jsonSecretKey = "f91e4494-04b3-4d49-8c27-57faed9e5785";
-
 
 const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
-let data = new FormData();
 
 app.post("/foodImage", (req, res) => {
+  let data = new FormData();
   data.append('image', fs.createReadStream(`C:/Users/bryce/Brainstation/bryce-borer-capstone/src/assets/images/${req.body.fileName}`), { encoding: null });
-  console.log(API_KEY)
+  console.log("API KEY ",API_KEY)
   console.log(req.body.fileName)
   let config = {
     method: 'post',
     maxBodyLength: Infinity,
-    url: 'https://vision.foodvisor.io/api/1.0/en/analysis/',
+    url: "https://vision.foodvisor.io/api/1.0/en/analysis/",
     headers: {
-      'Authorization': "Api-Key WGUmFqSK.gcxjKP1cm9F4MaLTZUlntSeuQWpHWreI",
+      'Authorization': `Api-Key ${API_KEY}`,
       ...data.getHeaders()
     },
     data: data
   };
 
-  // call to foodvisor API
-  // axios.request(config)
-  //   .then((response) => {
-  //     console.log(JSON.stringify(response.data));
-  //     res.send(JSON.stringify(response.data))
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   });
+  //call to foodvisor API
 
-  res.send(tempFood)
+  axios.request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      res.send(JSON.stringify(response.data))
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  //res.send(tempFood)
 });
 
 //Authentication
@@ -62,7 +61,7 @@ app.use((req, res, next) => {
 
     if (token) {
       console.log('Auth Token:', token);
-      if (jwt.verify(token, jsonSecretKey)) {
+      if (jwt.verify(token, "f91e4494-04b3-4d49-8c27-57faed9e5785")) {
         // Decode the token to pass along to end-points that may need
         // access to data stored in the token.
         req.decode = jwt.decode(token);
@@ -101,7 +100,7 @@ app.post("/login", (req, res) => {
   const user = users[username];
   if (user && user.password === password) {
     console.log('Found user:', user);
-    res.json({ token: jwt.sign({ name: user.name }, jsonSecretKey) });
+    res.json({ token: jwt.sign({ name: user.name }, "f91e4494-04b3-4d49-8c27-57faed9e5785") });
   } else {
     res.status(403).json({
       token: "",
