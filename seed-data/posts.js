1d9edf4812cb905b1f95c8ffaaa319148a7c88d7
Bryce Borer <bryce.borer@gmail.com>
2024-02-03 12:15:15 -0500

    fixed the seed data

--- seed-data/posts.js
+++ seed-data/posts.js
@@ -6,25 +6,28 @@ module.exports = [
         calories: 756,
         carbs: 164.7,
         protein: 15,
+        fat: 1.6,
         sugar: 16
     },
     {
         id: 2,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
+        food_name: "fruit salad",
+        portion: 150,
+        calories: 126,
+        carbs: 27,
         protein: 15,
-        sugar: 16
+        fat: 0.45,
+        sugar: 25.65
     },
     {
         id: 3,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Cheeseburger",
+        portion: 211,
+        calories: 552.82,
+        carbs: 53.59,
+        protein: 28.91,
+        fat: 24.27,
+        sugar: 11.07
     },
     {
         id: 4,
@@ -33,25 +36,28 @@ module.exports = [
         calories: 756,
         carbs: 164.7,
         protein: 15,
+        fat: 1.6,
         sugar: 16
     },
     {
         id: 5,
-        food_name: "cheerios",
+        food_name: "Miso soup",
         portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        calories: 70,
+        carbs: 6.06,
+        protein: 3.58,
+        fat: 2.74,
+        sugar: 1.8
     },
     {
         id: 6,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Bolognese lasagna",
+        portion: 227,
+        calories: 317.8,
+        carbs: 29.28,
+        protein: 15.55,
+        fat: 15.1,
+        sugar: 6.88
     },
     {
         id: 7,
@@ -60,25 +66,28 @@ module.exports = [
         calories: 756,
         carbs: 164.7,
         protein: 15,
+        fat: 1.6,
         sugar: 16
     },
     {
         id: 8,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Croque-monsieur",
+        portion: 164,
+        calories: 487.08,
+        carbs: 41.82,
+        protein: 23.78,
+        fat: 24.27,
+        sugar: 4.23
     },
     {
         id: 9,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Sheperds pie",
+        portion: 150,
+        calories: 217.5,
+        carbs: 13.61,
+        protein: 8.16,
+        fat: 14.39,
+        sugar: 7.67
     },
     {
         id: 10,
@@ -87,25 +96,28 @@ module.exports = [
         calories: 756,
         carbs: 164.7,
         protein: 15,
+        fat: 1.6,
         sugar: 16
     },
     {
         id: 11,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Cheese omelette",
+        portion: 112,
+        calories: 276.64,
+        carbs: 4.48,
+        protein: 20.05,
+        fat: 21.62,
+        sugar: 0.45
     },
     {
         id: 12,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Croque-monsieur",
+        portion: 164,
+        calories: 487.08,
+        carbs: 41.82,
+        protein: 23.78,
+        fat: 24.27,
+        sugar: 4.23
     },
     {
         id: 13,
@@ -114,25 +126,28 @@ module.exports = [
         calories: 756,
         carbs: 164.7,
         protein: 15,
+        fat: 1.6,
         sugar: 16
     },
     {
         id: 14,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
+        food_name: "fruit salad",
+        portion: 150,
+        calories: 126,
+        carbs: 27,
         protein: 15,
-        sugar: 16
+        fat: 0.45,
+        sugar: 25.65
     },
     {
         id: 15,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Sheperds pie",
+        portion: 150,
+        calories: 217.5,
+        carbs: 13.61,
+        protein: 8.16,
+        fat: 14.39,
+        sugar: 7.67
     },
     {
         id: 16,
@@ -141,25 +156,28 @@ module.exports = [
         calories: 756,
         carbs: 164.7,
         protein: 15,
+        fat: 1.6,
         sugar: 16
     },
     {
         id: 17,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Bolognese lasagna",
+        portion: 227,
+        calories: 317.8,
+        carbs: 29.28,
+        protein: 15.55,
+        fat: 15.1,
+        sugar: 6.88
     },
     {
         id: 18,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Croque-monsieur",
+        portion: 164,
+        calories: 487.08,
+        carbs: 41.82,
+        protein: 23.78,
+        fat: 24.27,
+        sugar: 4.23
     },
     {
         id: 19,
@@ -168,25 +186,28 @@ module.exports = [
         calories: 756,
         carbs: 164.7,
         protein: 15,
+        fat: 1.6,
         sugar: 16
     },
     {
         id: 20,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Cheese omelette",
+        portion: 112,
+        calories: 276.64,
+        carbs: 4.48,
+        protein: 20.05,
+        fat: 21.62,
+        sugar: 0.45
     },
     {
         id: 21,
-        food_name: "cheerios",
+        food_name: "Miso soup",
         portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        calories: 70,
+        carbs: 6.06,
+        protein: 3.58,
+        fat: 2.74,
+        sugar: 1.8
     },
     {
         id: 22,
@@ -195,25 +216,28 @@ module.exports = [
         calories: 756,
         carbs: 164.7,
         protein: 15,
+        fat: 1.6,
         sugar: 16
     },
     {
         id: 23,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Cheeseburger",
+        portion: 211,
+        calories: 552.82,
+        carbs: 53.59,
+        protein: 28.91,
+        fat: 24.27,
+        sugar: 11.07
     },
     {
         id: 24,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Bolognese lasagna",
+        portion: 227,
+        calories: 317.8,
+        carbs: 29.28,
+        protein: 15.55,
+        fat: 15.1,
+        sugar: 6.88
     },
     {
         id: 25,
@@ -222,24 +246,27 @@ module.exports = [
         calories: 756,
         carbs: 164.7,
         protein: 15,
+        fat: 1.6,
         sugar: 16
     },
     {
         id: 26,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Sheperds pie",
+        portion: 150,
+        calories: 217.5,
+        carbs: 13.61,
+        protein: 8.16,
+        fat: 14.39,
+        sugar: 7.67
     },
     {
         id: 27,
-        food_name: "cheerios",
-        portion: 200,
-        calories: 756,
-        carbs: 164.7,
-        protein: 15,
-        sugar: 16
+        food_name: "Cheeseburger",
+        portion: 211,
+        calories: 552.82,
+        carbs: 53.59,
+        protein: 28.91,
+        fat: 24.27,
+        sugar: 11.07
     },
-  ];
+];
